afc8754e8798473872ca8edf8fd7f928803b3cec
frostnn <gradysnn@icloud.com>
2020-07-20 18:49:27 +0300

    add ref NavItem component

--- src/components/AsideNav/AsideNav.jsx
+++ src/components/AsideNav/AsideNav.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import NavItem from "../NavItem/NavItem";
 import { navItem } from "../../data/asideInfo";
 import "./AsideNav.scss";
 
 const AsideNav = () => {
+  const [activeButton, setActiveButton] = useState(1);
+  const pickId = (id) => {
+    setActiveButton(activeButton(id));
+  };
   return (
     <div className="sidebar box sticky-column">
       <ul className="nav">
         {navItem.map((navItem) => {
           const { id, ...itemProps } = navItem;
-          return <NavItem {...itemProps} key={id} />;
+          return (
+            <NavItem
+              {...itemProps}
+              key={id}
+              active={activeButton === id}
+              cd={() => pickId(id)}
+            />
+          );
         })}
       </ul>
     </div>
--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -1,14 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavItem.scss";
-
-const NavItem = ({ link, icon, name, id }) => {
-  const fc = (e) => {
-    e.preventDefault();
-    return console.log(id);
-  };
+import style from "./active.scss";
+import { navItem } from "../../data/asideInfo";
+import concat from "../../units/concat";
+const NavItem = ({ link, icon, name, id, cb, active }) => {
   return (
     <li className="nav__item">
-      <a href={link} onClick={fc}>
+      <a href={link} onClick={cb} className={concat(active && style.active)}>
         {icon}
         {name}
       </a>
